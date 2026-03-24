b5e1488dca11f8956f2dec7d50ff8054c3bd912d
KaruriCreations <vincent.maina3@student.moringaschool.com>
2026-03-24 16:44:46 +0300

    Added the required Logic

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+const burgers = ["Hamburger", "Cheeseburger"]
+
+let featuredDrink = "Strawberry Milkshake"
+
+if(true){
+  const anotherNewBurger = "Maple Bacon Burger"
+  burgers.push(anotherNewBurger)
+}
+
+function addBurger(){
+    const newBurger = "Flatburger"
+    burgers.push(newBurger)
+}
+
+function changeFeaturedDrink(){
+    featuredDrink = "The JavaShake"
+}
